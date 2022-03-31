4868bc0a5b84598aa1d4ad190aa33f6285f3bfb3
Garam-Leee <grlee0806@gmail.com>
2022-03-31 14:15:14 +0900

    Hi

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// 데이터를 입력받아 화면에 표시할 내용을 반환하는 역할 (JSX를 사용)
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -11,7 +12,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// 앱에서 성능 측정을 시작하려면 함수를 전달하십시오.
+// 결과를 로그에 기록하려면(예: reportWebVitals(console.log))
+// 또는 분석 엔드포인트로 전송합니다. 상세: https://bit.ly/CRA-vitals
 reportWebVitals();
